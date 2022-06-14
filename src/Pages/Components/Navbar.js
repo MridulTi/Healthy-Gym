99991d09e03a5775e8d2fa8ad3f513e5db7f521e
MridulTi <mridultiwari2002@gmail.com>
2022-06-14 20:39:55 +0530

    Added Element and structure of the website

---
+++ src/Pages/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+export default function Nav(){
+    return(
+        <div>
+            <h1></h1>
+            <a href="#">Search Places</a>
+            <a href="#">StopWatch</a>
+            <a href="#">Add to Calendar</a>
+            <a href="#">Tips and Tricks</a>
+        </div>
+    )
+}
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Nav from "./Components/Navbar.js"
+import ReactDOM from "react-dom"
+export default function Home(){
+    return(
+        <div>
+            <Nav/>
+        </div>
+    )
+}
